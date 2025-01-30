d6eeb73fcd711243a698f3a51666cbb6854383a2
PaulHosang <paul_hosang@icloud.com>
2025-01-30 19:26:21 +0100

    Inhalt

--- lib/store.ts
+++ lib/store.ts
@@ -18,8 +18,8 @@ export const appState$ = observable<AppState>({
   attractions: [
     {
       id: "1",
-      name: "Attraction 1",
-      description: "This is the first attraction",
+      name: "NABU & Obst/Gartenbau Verein",
+      description: "Der Naturschutzbund Kleestadt und der Obst und Gartenbauverein machen zum dritten Mal einen Stand zusammen auf unserem Kläschter Dorffest. Hier werden Backfisch, Kartoffelsalat und Getränke angeboten.",
       position: [2, 1.5, -1.5],
     },
     {
